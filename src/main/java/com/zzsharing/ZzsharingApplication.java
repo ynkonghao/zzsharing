54edefb85c8415eea0a2537fa9c0aae5469dcbd0
ynkonghao <ynkonghao@gmail.com>
2017-03-01 22:35:25 +0800

    更改

--- src/main/java/com/zzsharing/ZzsharingApplication.java
+++ src/main/java/com/zzsharing/ZzsharingApplication.java
@@ -1,3 +1,5 @@
+package com.zzsharing;
+
 import com.zzsharing.repository.base.BaseRepositoryFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
