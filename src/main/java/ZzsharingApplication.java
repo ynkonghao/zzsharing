ac3ffdf154ec45ff50bdbf7673ce7ab2a4abd952
ynkonghao <ynkonghao@gmail.com>
2017-03-01 22:32:27 +0800

    完成了基础的repository

---
+++ src/main/java/ZzsharingApplication.java
@@ -0,0 +1,17 @@
+import com.zzsharing.repository.base.BaseRepositoryFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Created by konghao on 2016/12/15.
+ */
+@EnableJpaRepositories(basePackages = {"com.zzsharing"},
+        repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class//指定自己的工厂类
+)
+@SpringBootApplication
+public class ZzsharingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZzsharingApplication.class,args);
+    }
+}
